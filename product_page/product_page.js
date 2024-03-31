723d9e34c579d4ac302d77da93821e96d8c6acca
Nghiazz <87173058+MNghiazz@users.noreply.github.com>
2024-03-31 19:54:17 +0700

    almost done ui

--- product_page/product_page.js
+++ product_page/product_page.js
@@ -18,3 +18,97 @@ function changeTab(tabIndex) {
         document.getElementById('catalogContent').style.display = 'block';
     }
 }
+
+
+let thisPage = 1;
+let limit = 12;
+const list =  document.querySelectorAll('.catalog-content-wrap .volume-wrap .volume .chapters > li');
+
+function Loading() {
+    let beginGet = limit * (thisPage - 1);
+    let endGet = limit * thisPage - 1;
+    list.forEach((item, key) =>{
+        if(key >= beginGet && key <= endGet){
+            item.style.display = 'block';
+        }else {
+            item.style.display = 'none';
+        }
+    });
+    listPage(); 
+}
+Loading();
+
+
+
+function listPage() {
+    let count = Math.ceil(list.length / limit);
+    const listChapter = document.querySelector('.pagination');
+    listChapter.innerHTML = "";
+
+    if(thisPage != 1 && thisPage >= 4) {
+        let firstPage = document.createElement('li');
+        firstPage.innerHTML = `
+        <a aria-label="Previous">
+            <span aria-hidden="true">Trang đầu</span>
+        </a>
+        `;
+        firstPage.setAttribute('onclick', "changePage(" + 1 + ")");
+        listChapter.appendChild(firstPage);
+    }
+
+    if(thisPage != 1) {
+        let prev = document.createElement('li');
+        prev.innerHTML = `
+            <a aria-label="Previous">
+                <span aria-hidden="true">«</span>
+            </a>
+        `;
+        prev.setAttribute('onclick', "changePage(" + (thisPage - 1) + ")");
+        listChapter.appendChild(prev);
+    }
+    
+    for(i = 1; i <= count; i++) {
+        let newPage = document.createElement('li');
+        newPage.innerHTML = `
+            <a>
+                ${i}
+                <span class="sr-only" style>(current)</span>
+            </a>
+        `;
+        if(i == thisPage) {
+            newPage.classList.add('active');
+        }
+
+        newPage.setAttribute('onclick', "changePage(" + i + ")");
+        listChapter.appendChild(newPage);
+    }
+
+    if(thisPage != count) {
+        let next = document.createElement('li');
+        next.innerHTML = `
+            <a aria-label="Next">
+                <span aria-hidden="true">»</span>
+            </a>
+        `;
+        next.setAttribute('onclick', "changePage(" + (thisPage + 1) + ")");
+        listChapter.appendChild(next);
+    }
+
+    if(thisPage != count) {
+        let finalPage = document.createElement('li');
+        finalPage.innerHTML = `
+            <a  aria-label="Next">
+                <span aria-hidden="true">Trang cuối</span>
+            </a>
+        `;
+        finalPage.setAttribute('onclick', "changePage(" + (count) + ")");
+        listChapter.appendChild(finalPage);
+    }
+}
+
+function changePage(i) {
+    thisPage = i;
+    Loading();
+}
+
+
