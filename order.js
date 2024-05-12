9b8fd95bdd1e5924962e03f5ce5ae3fa487ccbfa
Nghiazz <87173058+MNghiazz@users.noreply.github.com>
2024-05-12 14:18:02 +0700

    .

--- order.js
+++ order.js
@@ -1,9 +1,30 @@
+
+
+const pending = document.querySelector('.pending-anchor');
+const borrowed = document.querySelector('.borrowed-anchor');
+const nyr = document.querySelector('.nyr-anchor');
+
+
+
+pending.addEventListener("click", function() {
+    changeTab(1);
+});
+borrowed.addEventListener("click", function() {
+    changeTab(2);
+});
+nyr.addEventListener("click", function() {
+    changeTab(3);
+});
+
+S
+
+
 function changeTab(tabIndex) {
     // Hide all content divs
 
-    document.querySelector('.nav-wrap ul li.act').classList.remove('act');
+    document.querySelector('.order-table .nav-wrap ul li.act').classList.remove('act');
 
-    document.querySelectorAll('.nav-wrap ul li')[tabIndex - 1].classList.add('act');
+    document.querySelectorAll('.order-table .nav-wrap ul li')[tabIndex - 1].classList.add('act');
 
     document.getElementById('pending').style.display = 'none';
     document.getElementById('borrowed').style.display = 'none';
@@ -14,24 +35,13 @@ function changeTab(tabIndex) {
     if (tabIndex === 1) {
 
         document.getElementById('pending').style.display = 'block';
-    }else if (tabIndex === 2) {
+    }
+    else if (tabIndex === 2) {
         
         document.getElementById('borrowed').style.display = 'block';
-    }else if (tabIndex === 3) {
+    }
+    else if (tabIndex === 3) {
         document.getElementById('not-yet-returned').style.display = 'block';
     }
 }
 
-const pending = document.querySelector('.order-table .pending');
-const borrowed = document.querySelector('.order-table .borrowed');
-const nyr = document.querySelector('.order-table .not-yet-returned');
-
-pending.addEventListener('click', function() {
-    changeTab(1);
-});
-borrowed.addEventListener('click', function() {
-    changeTab(2);
-});
-nyr.addEventListener('click', function() {
-    changeTab(3);
-});
